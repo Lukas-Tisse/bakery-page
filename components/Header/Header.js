e521125b17a057ccb2a809223d733afa69bce486
Lukas-Tisse <lukastisse3395@gmail.com>
2024-11-06 19:16:29 +0000

    add logo

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,14 @@
 import styles from "./Header.module.css";
-
+import Image from "next/image";
+import imageLogo from "../../public/imageLogo.png";
 function Header() {
   return (
     <div className={styles.header}>
-      <button className={styles.searchButton}>
-        <i class="bi bi-search"></i>
+      <div>
+        <Image src={imageLogo} className={styles.imageLogo} />
+      </div>
+      <button className={styles.userButton}>
+        <i class="bi bi-person-circle"></i>
       </button>
     </div>
   );
